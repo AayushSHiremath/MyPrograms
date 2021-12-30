ec6d9330e25d3cd5d26c91555bd03532906dd27a
Aayush S Hiremath <AayushSHiremath@gmail.com>
2021-12-30 09:32:54 +0000

    Updated Jarvis Virtual Assistant

--- Jarvis_Virtual_Assistant.py
+++ Jarvis_Virtual_Assistant.py
@@ -61,6 +61,33 @@ My_Joke_List = ["can a kangaroo jump higher than a house , of course , a house c
             "Why cant a bicycle stand on its own? It is two tired.",
             "Can February March? No but April May"]
 
+My_WhoAreYou_List = ["I am Jarvis , Your Personal Assistant , I am here to help , let me know if you need anything", "I am Jarvis your virtual Assistant , I can play music , tell you what the time is , all you need to do is ask"]
+
+My_WhereAreYouFrom_List = ["First I was an Idea , Then someone did a lot of experiments on me , and that's where i came from", "I come from the headquarters of apple company , just kidding I am from A house in Banglore."]
+
+My_WhoMadeYou_List = ["I was created by elon musk , Just kidding I was made by an awesome kid who knows how to code", "I was born by a bright mind who started working to create an Assistant , Just for you"]
+
+My_Hello_List = ["Hello sir , how are you", "Hello Sir , Didnt see you there.", "Hey , Whats up", "Hey there , I was busy planning New year's eve , I am very exicted."]
+
+My_ThankYou_List = ["You are very welcome sir", "I am honoured to serve.", "No worries , I am here to help", "Just doing my job"]
+
+My_WhatCanYouDo_List = ["I can do many things , some of them are Open youtube , what is the time.", "I can do many things , and I'm learning more , You can ask me to do things like open youtube , or show a video on youtube", "Here are some things you can try , open gmail or show me updatess on 8 bitryan."]
+
+My_HowHasYourDayBeen_List = ["I'm splendid , thank you for asking", "Great , Thanks", "Its been good , you are very kind to ask , especially in these times"]
+
+My_TellMeYourSecret_List = ["Here's a secret, I am not actually a person", "Okay but keep it to yourself , Sometimes I just pretend to understand sarcasm"]
+
+My_ILoveYou_List = ["You should know the feelings mutual", "no wonder , If I was you , I would love me too", "I love me too"]
+
+My_YouAreTheBest_List = ["That's so funny , i was just thinking the same thing about you", "Thats nice to hear , I hope you know the fellings mutual"]
+
+My_WhatCanYouNotDo_List = ["I cant give you a Hi-fi , but i still think you are great", "I haven't tested this , But I am pretty sure i can't swim", "I cant really handle any spicy food , or any food", "I am not very good at pull ups"]
+
+My_HowDoYouDoThat_List = ["It was either magic , or good programming", "Magic", "shh , it's a secret", "I am not sure"]
+
+
+
+
 
 
 def Listen():
@@ -85,68 +112,57 @@ def Listen():
                 engine.runAndWait()  
 
             elif 'who are you' in command:
-                engine.say("I am Jarvis , Your Personal Assistant , I am here to help , let me know if you need anything")
-                engine.runAndWait()
-                #I am Jarvis your virtual Assistant , I can play music , tell you what the time is , all you need to do is ask
-                
+                engine.say(random.choice(My_WhoAreYou_List))
+                engine.runAndWait()                
 
             elif 'where are you from' in command:
-                engine.say("I come from the headquarters of apple company , just kidding I am from A house in Banglore.")
+                engine.say(random.choice(My_WhereAreYouFrom_List))
                 engine.runAndWait()
-                #First I was an Idea , Then a huge group of 1 person did a lot of experiments on me , and that's where i came from
 
             elif 'who made you' in command:
-                engine.say("I was created by elon musk , Just kidding I was made by an awesome kid who knows how to code")
-                engine.runAndWait()
-                #I was born by a bright mind who started working to create an Assistant , Just for you
+                engine.say(random.choice(My_WhoMadeYou_List))
+                engine.runAndWait()     
 
             elif ('hello jarvis' in command) or ('good morning' in command) or ('good afternoon' in command) or ('good evening' in command):
-                engine.say("Hello sir , how are you")
+                engine.say(random.choice(My_Hello_List))
                 engine.runAndWait()
-                #Hello Sir , Didnt see you there.
-                #Hey , Whats up
-                #Hey there , I was busy planning New year's eve , I am very exicted.
 
             elif ('thank you' in command) or ('thank you so much' in command):
-                engine.say("You are very welcome sir")
+                engine.say(random.choice(My_ThankYou_List))
                 engine.runAndWait()
-                #I am honoured to serve.
-                #No worries , I am here to help
-                #Just doing my job
 
             elif ('what can you do' in command) or ('what are your features' in command) or ('Can you help me' in command):
-                engine.say("I can do many things , some of them are Open youtube , what is the time , what is the weather , etc.")
-                engine.runAndWait()
-                #
-
-            elif 'how has your day been' in command:
-                engine.say('Its been good , you are very kind to ask , especially in these times')
+                engine.say(random.choice(My_WhatCanYouDo_List))
                 engine.runAndWait()
 
-            elif 'tell me your secret' in command:
-                engine.say("Okay but keep it to yourself , Sometimes I just pretend to understand sarcasm")
+            elif ('how has your day been' in command) or ('how was your day' in command):
+                engine.say(random.choice(My_HowHasYourDayBeen_List))
                 engine.runAndWait()
 
-            elif 'i hate you' in command:
+            elif ('tell me your secret' in command) or ('tell me a secret' in command):
+                engine.say(random.choice(My_TellMeYourSecret_List))
+                engine.runAndWait()
+
+            elif ('i hate you' in command) or ('you are the worst' in command):
                 engine.say("Well i'm still learning , what do we need to fix")
                 engine.runAndWait()
 
             elif 'i love you' in command:
-                engine.say("I love me too")
+                engine.say(random.choice(My_ILoveYou_List))
                 engine.runAndWait()
 
             elif 'you are the best' in command:
-                engine.say("That's so funny , i was just thinking the same thing about you")
+                engine.say(random.choice(My_YouAreTheBest_List))
                 engine.runAndWait()
 
             elif 'What can you not do' in command:
-                engine.say("I cant give you a Hi-fi , but i still think you are great")
+                engine.say(random.choice(My_WhatCanYouNotDo_List))
                 engine.runAndWait()
                 
             elif ('how do you do that' in command) or ('how did you do that' in command):
-                engine.say("It was either magic , or good programming")
+                engine.say(random.choice(My_HowDoYouDoThat_List))
                 engine.runAndWait()
-
+            
             elif 'tell me a joke' in command:
                 engine.say(random.choice(My_Joke_List))
                 engine.runAndWait()
@@ -155,6 +171,18 @@ def Listen():
                 engine.say("the most famous youtuber in the world is Pewdiepie")
                 engine.runAndWait()
 
+            elif 'self destruct' in command:
+                engine.say("Self destructing in 3, 2, 1         Actually I think I will stick around")
+                engine.runAndWait()
+
+             elif 'do you breathe' in command:
+                engine.say("I dont , which means I never have to worry about bad breath.")
+                engine.runAndWait()
+            
+             elif 'can you eat' in command:
+                engine.say("My device is all charged up , i am ready to help")
+                engine.runAndWait()
+        
             #Opening Apps
 
             elif ('open youtube' in command) or ('startup youtube' in command):
